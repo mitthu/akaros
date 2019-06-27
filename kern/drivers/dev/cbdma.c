ad656438d1739548abe13d7d2e1fd1ad34672ca3
Aditya Basu <mitthu@google.com>
2019-06-26 20:35:33 -0400

    cbdma: Add "stats" and initialize the PCI device

--- kern/drivers/dev/cbdma.c
+++ kern/drivers/dev/cbdma.c
@@ -1,34 +1,51 @@
 /*
- * File: cbdma.c - Driver for accessing Intel CBDMA
+ * File: cbdma.c - Driver for using Intel CBDMA
  * Author: Aditya Basu <mitthu@google.com>
  */
 
-#include <slab.h>
 #include <kmalloc.h>
-#include <kref.h>
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
 #include <error.h>
-#include <cpio.h>
-#include <pmap.h>
-#include <smp.h>
 #include <net/ip.h>
-#include <arch/io.h>
+#include <linux_compat.h>
+#include <arch/pci.h>
+#include <cbdma_regs.h>
 
-struct dev cbdmadevtab;
-
+struct dev                      cbdmadevtab;
+static struct pci_device        *pci;
+static char                     *mmio;
+static uint32_t                 mmio_sz;
 /* QID Path */
 enum {
         Qdir           = 0,
         Qcbdmaktest    = 1,
+        Qcbdmastats    = 2,
 };
 
 static struct dirtab cbdmadir[] = {
         {".",         {Qdir, 0, QTDIR}, 0, 0555},
         {"ktest",     {Qcbdmaktest, 0, QTFILE}, 0, 0555},
+        {"stats",     {Qcbdmastats, 0, QTFILE}, 0, 0555},
 };
 
+/* Descriptor structue as defined in the programmer's guide.
+ * It describes a single DMA transfer
+ */
+struct desc {
+  uint32_t  xfer_size;
+  uint32_t  descriptor_control;
+  uint64_t  src_addr;
+  uint64_t  dest_addr;
+  uint64_t  next_desc_addr;
+  uint64_t  next_source_address;
+  uint64_t  next_destination_address;
+  uint64_t  reserved0;
+  uint64_t  reserved1;
+} __attribute__((packed));
+
+/* Function definitions start here */
 static char *devname(void) {
         return cbdmadevtab.name;
 }
@@ -61,6 +78,36 @@ static struct chan *cbdmaopen(struct chan *c, int omode) {
 static void cbdmaclose(struct chan *_) {
 }
 
+/* cbdma_ktest: performs functional test on CBDMA
+   
+ - Allocates 2 kernel pages: src and dest.
+ - memsets the src page
+ - Prepare descriptors for DMA transfer
+ - Initiate the transfer
+ - Verify results
+ - Print stats
+ */ 
+static size_t cbdma_ktest(struct chan *c, void *va, size_t n, off64_t offset) {
+        char *str = "Not implemented yet!\n";
+
+        return readstr(offset, va, n, str);
+}
+
+/* cbdma_stats: some stats about the driver
+ */ 
+static size_t cbdma_stats(struct chan *c, void *va, size_t n, off64_t offset) {
+        char buf[4096];
+        char *ebuf = buf + sizeof(buf) - 1; /* -1 for newline */
+        char *iter = buf;
+
+        iter = seprintf(iter, ebuf, "Intel CBDM [%x:%x] mmio:%x mmio_sz:%lu",
+                        pci->ven_id, pci->dev_id, mmio, mmio_sz);
+
+        *iter++ = '\n';
+        *iter   = '\0';
+        return readstr(offset, va, n, buf);
+}
+
 static size_t cbdmaread(struct chan *c, void *va, size_t n, off64_t offset) {
         switch (c->qid.path) {
         case Qdir:
@@ -68,65 +115,59 @@ static size_t cbdmaread(struct chan *c, void *va, size_t n, off64_t offset) {
                                   ARRAY_SIZE(cbdmadir), devgen);
 
         case Qcbdmaktest:
-                error(EPERM, "Reading is not yet implemented!");
+                return cbdma_ktest(c, va, n, offset);
+
+        case Qcbdmastats:
+                return cbdma_stats(c, va, n, offset);
 
         default:
-                panic("cbdmaread: qid %d is impossible", c->qid.path);
+                panic("cbdmaread: qid 0x%x is impossible", c->qid.path);
         }
 
         return -1;      /* not reached */
 }
 
 static size_t cbdmawrite(struct chan *c, void *va, size_t n, off64_t offset) {
-        error(EPERM, "Writing is not yet implemented!");
-        return -1;
-        // uint8_t *a;
-        // int i, r, tbdf;
-        // uint32_t x;
-        // struct pci_device *p;
+        panic("Not implemented yet!");
 
-        // if (n > PCI_CONFIG_SZ)
-        //         n = PCI_CONFIG_SZ;
-        // a = va;
+        return 0;
+}
 
-        // switch (TYPE(c->qid)) {
-        // case Qpciraw:
-        //         tbdf = MKBUS(BusPCI, 0, 0, 0) | BUSBDF((uint32_t) c->qid.path);
-        //         p = pci_match_tbdf(tbdf);
-        //         if (p == NULL)
-        //                 error(EINVAL, ERROR_FIXME);
-        //         if (offset > PCI_CONFIG_SZ)
-        //                 return 0;
-        //         if (n + offset > PCI_CONFIG_SZ)
-        //                 n = PCI_CONFIG_SZ - offset;
-        //         r = offset;
-        //         if (!(r & 3) && n == 4) {
-        //                 x = GBIT32(a);
-        //                 pcidev_write32(p, r, x);
-        //                 return 4;
-        //         }
-        //         if (!(r & 1) && n == 2) {
-        //                 x = GBIT16(a);
-        //                 pcidev_write16(p, r, x);
-        //                 return 2;
-        //         }
-        //         for (i = 0; i < n; i++) {
-        //                 x = GBIT8(a);
-        //                 pcidev_write8(p, r, x);
-        //                 a++;
-        //                 r++;
-        //         }
-        //         return i;
-        // default:
-        //         error(EINVAL, ERROR_FIXME);
-        // }
-        // return n;
+void cbdmainit(void) {
+        int tbdf  = MKBUS(BusPCI, 0, 0x4, 0);
+        pci       = NULL;
+        mmio      = NULL;
+        mmio_sz   = -1;
+        int i;
+
+        /* search for the device 00:04.0 */
+        pci = pci_match_tbdf(tbdf);
+        if (pci == NULL) {
+                error(EINVAL, "Intel CBDMA PCI device not found\n");
+                return;
+        }
+
+        /* search and find the mapped mmio region */
+        for (i = 0; i < COUNT_OF(pci->bar); i++) {
+                if (pci->bar[i].mmio_sz == 0)
+                        continue;
+                mmio    = (char *) pci->bar[i].mmio_base64;
+                mmio_sz = pci->bar[i].mmio_sz;
+                break;
+        }
+
+        if (mmio == NULL || mmio_sz == -1) {
+                error(EINVAL, "Cannot register Intel CBDMA\n");                
+        }
+
+        printk(KERN_INFO "Registered: Intel CBDM [%x:%x] mmio:%x mmio_sz:%lu\n",
+                pci->ven_id, pci->dev_id, mmio, mmio_sz);
 }
 
 struct dev cbdmadevtab __devtab = {
         .name       = "cbdma",
         .reset      = devreset,
-        .init       = devinit,
+        .init       = cbdmainit,
         .shutdown   = devshutdown,
         .attach     = cbdmaattach,
         .walk       = cbdmawalk,
