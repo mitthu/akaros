5761ea562a58ad0f74dc0815667f14953f7e474c
Aditya Basu <mitthu@google.com>
2019-08-13 16:33:54 -0400

    iommu: add fields for iotlb shootouts

--- kern/arch/x86/iommu.h
+++ kern/arch/x86/iommu.h
@@ -55,7 +55,11 @@ struct iommu {
 	struct proc_list procs; // unused
 	bool supported;
 	bool using_qemu;
+	bool device_iotlb;
+	bool rwbf;
 
+	uint16_t iotlb_cmd_offset;
+	uint16_t iotlb_addr_offset;
 	void __iomem *regio;
 	uint64_t rba; /* for unique assertion */
 	uint64_t num_assigned_devs;
